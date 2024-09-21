40808b5afe5d1879eab5b3a7d3b3c1b0c343b469
Ethan <ethan_here@proton.me>
2024-09-21 17:01:47 -0500

    Add car generation loop and subsequent cleaning

--- game_objects.py
+++ game_objects.py
@@ -1,8 +1,36 @@
 from turtle import *
+from random import *
+
+# Constants
+CAR_SPEED = 3
+CAR_COLORS = ["red", "blue", "green", "orange", "purple", "pink", "yellow", "cyan", "magenta", "brown", "gray", "lime"]
 
 class Car():
     def __init__(self) -> None:
-        pass
+        self.car_obj = Turtle()
+        self.current_y = 0
+
+        self.design_car()
+        self.determine_spawn()
+    
+    def design_car(self) -> None:
+        self.car_obj.shape("square")
+        self.car_obj.shapesize(1, 2)
+        self.car_obj.pu()
+        self.car_obj.color(choice(CAR_COLORS))
+
+    def determine_spawn(self) -> None:
+        self.current_y = randrange(-300, 300)
+        self.car_obj.goto(-400, self.current_y)
+
+    def car_move(self) -> bool:
+        new_x = self.car_obj.xcor() + CAR_SPEED
+        self.car_obj.goto(new_x, self.current_y)
+
+        if(self.car_obj.xcor() > 400):
+            return True
+        return False
+
 
 class Player():
     def __init__(self) -> None:
--- game_settings.py
+++ game_settings.py
@@ -1,12 +1,48 @@
 from turtle import *
+from game_objects import *
+
+# Constants
+GAME_SPEED = 20
+CAR_SPAWN_TIMER = 350
+TOTAL_CARS_ON_SCREEN = 20
 
 class UI():
     def __init__(self) -> None:
         self.game_window = Screen()
+        self.game_window.tracer(0)
+        
+    def update_screen(self) -> None:
+        self.game_window.update()
 
 class Game():
     def __init__(self) -> None:
         self.ui = UI()
+        self.current_cars = []
+        self.cars_to_be_removed = []
+        self.spawn_car()
+
+        self.game_loop()
+
+    def game_loop(self):
+        for car in self.current_cars:
+            if(car.car_move()):
+                self.cars_to_be_removed.append(car)
+        self.clean_cars()
+        self.ui.update_screen()
+        self.ui.game_window.ontimer(self.game_loop, GAME_SPEED)
+
+    def spawn_car(self):
+        self.ui.game_window.ontimer(self.spawn_car, CAR_SPAWN_TIMER)
+
+        if len(self.current_cars) < TOTAL_CARS_ON_SCREEN:
+            car = Car()
+            self.current_cars.append(car)
+
+    def clean_cars(self):
+        for car in self.cars_to_be_removed:
+            self.current_cars.remove(car)
+            car.car_obj.hideturtle()
+        self.cars_to_be_removed.clear()
 
     def run(self):
         self.ui.game_window.mainloop()
