a695098276b2129863ede29acf8031f429c78813
Ethan <ethan_here@proton.me>
2024-09-28 12:49:36 -0500

    Removed duplicated lines

--- game_objects.py
+++ game_objects.py
@@ -3,7 +3,6 @@ from random import *
 
 # Constants
 CAR_SPEED = 5
-CAR_SPEED = 5
 CAR_COLORS = ["red", "blue", "green", "orange", "purple", "pink", "cyan", "magenta", "brown", "gray", "lime"]
 PLAYER_STARTING_Y = -350
 PLAYER_SPEED = 2
@@ -13,7 +12,6 @@ class Car():
     def __init__(self) -> None:
         self.car_obj = Turtle()
         self.current_y = 0
-        self.direction = ""
         self.direction = ""
 
         self.collision_min_x = 0
@@ -42,49 +40,19 @@ class Car():
         elif lane[0] == 305:
             self.direction = "left"
             spawn_x = screen.window_width() / 2 + 40
-            
-        self.car_obj.goto(spawn_x, self.current_y)
-    def determine_spawn(self, screen, spawn_points) -> None:
-        lane = choice(spawn_points)
-        self.current_y = choice(lane)
 
-        if lane[0] == 245:
-            self.direction = "right"
-            spawn_x = -screen.window_width() / 2 - 40
-
-        elif lane[0] == 305:
-            self.direction = "left"
-            spawn_x = screen.window_width() / 2 + 40
-            
         self.car_obj.goto(spawn_x, self.current_y)
 
     def car_move(self, screen) -> bool:
-        new_x = 0
-
         if self.direction == "right":
             new_x = self.car_obj.xcor() + CAR_SPEED
-            if(self.car_obj.xcor() > screen.window_width() / 2 + 40):
+            if self.car_obj.xcor() > screen.window_width() / 2 + 40:
                 print(f"{self.car_obj} deleted.")
                 return True
 
         elif self.direction == "left":
             new_x = self.car_obj.xcor() - CAR_SPEED
-            if(self.car_obj.xcor() < -screen.window_width() / 2 - 40):
-                print(f"{self.car_obj} deleted.")
-                return True
-
-    def car_move(self, screen) -> bool:
-        new_x = 0
-
-        if self.direction == "right":
-            new_x = self.car_obj.xcor() + CAR_SPEED
-            if(self.car_obj.xcor() > screen.window_width() / 2 + 40):
-                print(f"{self.car_obj} deleted.")
-                return True
-
-        elif self.direction == "left":
-            new_x = self.car_obj.xcor() - CAR_SPEED
-            if(self.car_obj.xcor() < -screen.window_width() / 2 - 40):
+            if self.car_obj.xcor() < -screen.window_width() / 2 - 40:
                 print(f"{self.car_obj} deleted.")
                 return True
 
@@ -104,10 +72,8 @@ class Player():
         self.player_obj = Turtle()
         self.player_obj.shape("turtle")
         self.player_obj.color("green3")
-        self.player_obj.color("green3")
         self.player_obj.pu()
         self.player_obj.lt(90)
-        self.player_obj.shapesize(PLAYER_SIZE, PLAYER_SIZE)
         self.player_obj.shapesize(PLAYER_SIZE, PLAYER_SIZE)
         self.player_obj.goto(0, PLAYER_STARTING_Y)
 
@@ -123,7 +89,6 @@ class Player():
     def move(self) -> None:
         new_y = self.player_obj.ycor() + self.y_velocity
         self.player_obj.goto(0, new_y)
-
         self.update_collision()
 
     def update_collision(self) -> None:
@@ -132,12 +97,9 @@ class Player():
 
     def check_for_collision(self, current_cars) -> bool:
         for car in current_cars:
-            if abs(self.player_obj.xcor() - car.car_obj.xcor() < 50):
-                within_x_range = self.player_min_x <= (car.collision_min_x+40) and (self.player_min_x+30) >= car.collision_min_x
-                within_y_range = self.player_min_y <= (car.collision_min_y+20) and (self.player_min_y+30) >= car.collision_min_y
-            if abs(self.player_obj.xcor() - car.car_obj.xcor() < 50):
-                within_x_range = self.player_min_x <= (car.collision_min_x+40) and (self.player_min_x+30) >= car.collision_min_x
-                within_y_range = self.player_min_y <= (car.collision_min_y+20) and (self.player_min_y+30) >= car.collision_min_y
+            if abs(self.player_obj.xcor() - car.car_obj.xcor()) < 50:
+                within_x_range = self.player_min_x <= (car.collision_min_x + 40) and (self.player_min_x + 30) >= car.collision_min_x
+                within_y_range = self.player_min_y <= (car.collision_min_y + 20) and (self.player_min_y + 30) >= car.collision_min_y
 
                 if within_x_range and within_y_range:
                     print("Collision detected!")
