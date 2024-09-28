26d76eb7b63af71803884ae4fe4eaa25cedc537d
Ethan <ethan_here@proton.me>
2024-09-28 12:48:00 -0500

    Git moment

--- game_settings.py
+++ game_settings.py
@@ -1,12 +1,8 @@
 from turtle import *
 from game_objects import *
 from random import randint
-from random import randint
 
 # Constants
-GAME_SPEED = 15
-CAR_SPAWN_TIMER = 450
-TOTAL_CARS_ON_SCREEN = 30
 GAME_SPEED = 15
 CAR_SPAWN_TIMER = 450
 TOTAL_CARS_ON_SCREEN = 30
@@ -18,28 +14,19 @@ LINE_WIDTH = 10
 LINE_LENGTH = 60
 FINISH_LINE_Y = 360
 SPAWN_POINT_OFFSET = 30
-ROAD_LENGTH = 550
-ROAD_WIDTH = 110
-LINE_WIDTH = 10
-LINE_LENGTH = 60
-FINISH_LINE_Y = 360
-SPAWN_POINT_OFFSET = 30
 
 class UI():
     def __init__(self) -> None:
         self.game_window = Screen()
         self.game_window.setup(1024, 768)
-
-        # Adjust for FPS
-        self.game_window.tracer(20)
+        self.game_window.tracer(20)  # Adjust for FPS
 
         self.gameplay_turtle = Turtle()
-        self.draw_finish_line()
-
         self.text_turtle = Turtle()
 
+        self.draw_finish_line()
         self.roads = Road_Builder()
-        
+
     def update_screen(self) -> None:
         self.game_window.update()
 
@@ -53,11 +40,11 @@ class UI():
     def draw_finish_line(self) -> None:
         self.gameplay_turtle.pen(pendown=False)
         self.gameplay_turtle.goto(-ROAD_LENGTH, FINISH_LINE_Y)
-        self.gameplay_turtle.pen(pendown=True, pencolor="green2", pensize=10, shown=False)
+        self.gameplay_turtle.pen(pendown=True, pencolor="green2", pensize=10)
         self.gameplay_turtle.goto(ROAD_LENGTH, FINISH_LINE_Y)
 
     def draw_words(self, string, var_color) -> None:
-        self.text_turtle.pen(pendown=False, shown=False)
+        self.text_turtle.pen(pendown=False)
         self.text_turtle.goto(0, 170)
         self.text_turtle.pen(pendown=True, pencolor=var_color)
         self.text_turtle.write(f"{string}", align="center", font=("Courier", 32, "bold"))
@@ -68,38 +55,30 @@ class UI():
 class Road_Builder():
     def __init__(self) -> None:
         self.road_builder = Turtle()
-
         self.left_lane_spawn_points = []
         self.right_lane_spawn_points = []
         self.lane_spawn_points = [self.left_lane_spawn_points, self.right_lane_spawn_points]
-
         self.draw_roads()
 
     def draw_roads(self) -> None:
         roads_needed = 4
         road_y = 275
-
         while roads_needed > 0:
             self.draw_individual_road(0, road_y)
             road_y -= 175
             roads_needed -= 1
+        self.road_builder.pen(pendown=False)
 
-        self.road_builder.pen(pendown=False, shown=False)
-    
     def draw_individual_road(self, x, y) -> None:
         self.road_builder.pen(pendown=False, pencolor="gray30", pensize=ROAD_WIDTH)
-
-        self.road_builder.goto(x-ROAD_LENGTH, y)
-        self.road_builder.pd()
-        self.road_builder.goto(x+ROAD_LENGTH, y)
-
+        self.road_builder.goto(x - ROAD_LENGTH, y)
+        self.road_builder.pendown()
+        self.road_builder.goto(x + ROAD_LENGTH, y)
         self.update_spawn_points(y)
-
         self.draw_lines()
 
     def draw_lines(self) -> None:
         self.road_builder.pen(pencolor="white", pensize=LINE_WIDTH)
-
         random_starting_x = self.road_builder.xcor() + randint(-40, 40)
         self.road_builder.goto(random_starting_x, self.road_builder.ycor())
         lines = 20
@@ -107,11 +86,10 @@ class Road_Builder():
         while lines > 0:
             self.road_builder.pen(pendown=draw)
             draw = not draw
-
             new_x = self.road_builder.xcor() - LINE_LENGTH
             self.road_builder.goto(new_x, self.road_builder.ycor())
             lines -= 1
-            
+
     def update_spawn_points(self, spawn_point):
         self.left_lane_spawn_points.append(spawn_point - SPAWN_POINT_OFFSET)
         self.right_lane_spawn_points.append(spawn_point + SPAWN_POINT_OFFSET)
@@ -125,30 +103,24 @@ class Game():
 
         self.win = False
         self.collision_detected = False
-
-        # Presently unused
-        self.difficulty = 1
-
         self.player = Player()
 
         self.ui.begin_listening(self.player)
-
         self.game_loop()
 
     def game_loop(self) -> None:
         for car in self.current_cars:
-            if(car.car_move(self.ui.game_window)):
-            if(car.car_move(self.ui.game_window)):
+            if car.car_move(self.ui.game_window):
                 self.cars_to_be_removed.append(car)
 
         if not self.win and not self.collision_detected:
             self.collision_detected = self.player.check_for_collision(self.current_cars)
             self.win = self.player.check_for_finish(FINISH_LINE_Y)
             self.player.move()
-        
+
         if self.collision_detected:
             self.hit_by_car()
-        
+
         if self.win:
             self.reached_end()
 
@@ -161,10 +133,8 @@ class Game():
 
     def spawn_car(self) -> None:
         self.ui.game_window.ontimer(self.spawn_car, CAR_SPAWN_TIMER)
-
         if len(self.current_cars) < TOTAL_CARS_ON_SCREEN:
             car = Car()
-            car.determine_spawn(self.ui.game_window, self.ui.roads.lane_spawn_points)
             car.determine_spawn(self.ui.game_window, self.ui.roads.lane_spawn_points)
             self.current_cars.append(car)
 
@@ -173,9 +143,8 @@ class Game():
             self.current_cars.remove(car)
             car.car_obj.hideturtle()
             del car
-            del car
         self.cars_to_be_removed.clear()
-    
+
     def reached_end(self):
         self.ui.draw_words("You Win!", "black")
 
